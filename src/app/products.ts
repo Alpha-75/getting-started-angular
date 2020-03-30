8f0758cd93c36f9ade00a78865456ddc60001c4e
POPULUS <pascalpopulus@free.fr>
2020-03-30 17:54:01 +0200

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  },
+  {
+    name: 'Phone OnePlus 6T',
+    price: 499,
+    description: 'The best existing phone'
+  }
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
